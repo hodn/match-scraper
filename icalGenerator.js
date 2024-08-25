2783832d9773bd6e053543c2bd95060ed1af761d
Hoang Doan <hoang.doan@rocketmail.com>
2024-08-26 01:42:57 +0200

    referees + adjusted game time

--- icalGenerator.js
+++ icalGenerator.js
@@ -1,23 +1,24 @@
-module.exports = function icalGenerator(url, matches) {
+module.exports = function icalGenerator(url, events) {
 
     const ical = require('ical-generator');
 
     const calendar = ical({ name: 'PSMF zapasy' });
     calendar.timezone('Europe/Prague');
 
-    for (let index = 0; index < matches.length; index++) {
-        let selected = matches[index];
+    for (let index = 0; index < events.length; index++) {
+        let selected = events[index];
 
         let date = selected.date.split('.');
         let time = selected.time.split(':');
         const startTime = new Date('20' + date[2], date[1] - 1, date[0], time[0], time[1]);
-        const endTime = new Date('20' + date[2], date[1] - 1, date[0], time[0], time[1]);
-        endTime.setHours(endTime.getHours() + 1);
+        const endTime = new Date();
+        endTime.setTime(startTime.getTime() + 65 * 60 * 1000);
 
+        
         calendar.createEvent({
             start: startTime,
             end: endTime,
-            summary: `${selected.homeTeam} - ${selected.awayTeam}`,
+            summary: selected.isRefType ? `Pískání: ${selected.homeTeam} - ${selected.awayTeam}` : `${selected.homeTeam} - ${selected.awayTeam}`,
             description: {
                 plain: `${selected.homeTeam}: ${selected.homeColors} \n${selected.awayTeam}: ${selected.awayColors} \n${selected.round} kolo \nHřiště: ${selected.pitch} \nRozpis: ${url}`,
                 html: `${selected.homeTeam}: ${selected.homeColors} <br/> ${selected.awayTeam}: ${selected.awayColors} <br/> ${selected.round} kolo <br/> Hřiště: ${selected.pitch} <br/> Rozpis: $url`
